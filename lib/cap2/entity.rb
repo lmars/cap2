5dd77707e60f0cc97d2906bda383bf7187b6e04e
Lewis Marshall <lewismarshall86@gmail.com>
2012-08-15 21:32:58 +0100

    Refactor Entity by adding Entity#has?

--- lib/cap2/entity.rb
+++ lib/cap2/entity.rb
@@ -1,15 +1,22 @@
 module Cap2
   class Entity
     def permitted?(capability)
-      Cap2.has_capability?(@entity_id, Cap2::PERMITTED, capability)
+      has?(:permitted, capability)
     end
 
     def effective?(capability)
-      Cap2.has_capability?(@entity_id, Cap2::EFFECTIVE, capability)
+      has?(:effective, capability)
     end
 
     def inheritable?(capability)
-      Cap2.has_capability?(@entity_id, Cap2::INHERITABLE, capability)
+      has?(:inheritable, capability)
+    end
+
+    private
+    def has?(set, cap)
+      set = Cap2.const_get(set.to_s.upcase)
+
+      Cap2.has_capability?(@entity_id, set, cap)
     end
   end
 end
