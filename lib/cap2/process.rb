9ce475a20b9e3bf85d0fa676ab4ac9f6c3fe48dc
Lewis Marshall <lewismarshall86@gmail.com>
2012-08-19 18:59:35 +0100

    Document Process.new

--- lib/cap2/process.rb
+++ lib/cap2/process.rb
@@ -1,13 +1,15 @@
+# = Cap2::Process
+#
+# A class with methods for querying capabilities for the
+# process with pid provided to the initialize method.
+
 require 'cap2/set_methods'
 
 module Cap2
   class Process
-    # = Cap2::Process
-    #
-    # A class with methods for querying capabilities for the
-    # process with pid provided to the initialize method.
     include SetMethods
 
+    # Initialize a new Process object for the given pid.
     def initialize(pid)
       @pid = pid
     end
