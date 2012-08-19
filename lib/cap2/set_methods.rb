46a3a6bfaeb5720183220a5306182225a1591785
Lewis Marshall <lewismarshall86@gmail.com>
2012-08-19 20:00:20 +0100

    Document methods in SetMethods

--- lib/cap2/set_methods.rb
+++ lib/cap2/set_methods.rb
@@ -7,14 +7,17 @@ module Cap2
   # cased name of a capability, without the 'CAP_' prefix.
   # For example, :chown would query the CAP_CHOWN capability.
   module SetMethods
+    # Returns whether the given capability is permitted
     def permitted?(capability)
       has?(:permitted, capability)
     end
 
+    # Returns whether the given capability is effective
     def effective?(capability)
       has?(:effective, capability)
     end
 
+    # Returns whether the given capability is inheritable
     def inheritable?(capability)
       has?(:inheritable, capability)
     end
